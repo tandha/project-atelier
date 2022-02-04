bed88d54347cf487f9b25d102c7bda00119d943e
Joe Mitzman <joemitz@gmail.com>
2022-02-04 15:58:25 -0800

    Completed installation and configuration of babel and webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './client/src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'client', 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: [
+          path.resolve(__dirname, 'client', 'src')
+        ],
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map'
+}
