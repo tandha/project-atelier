17063c1aa90a2b9d1f498c5201b77ebb7f77cb27
Caitlin Winters <caitlin.p.winters@gmail.com>
2022-02-05 18:21:44 -0500

    Added boilerplate express server code and accepted incoming changes to .gitignore 😬

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, '/../client/dist')));
+
+
+app.listen(port, ()=> {
+  console.log(`Listening at localhost:${port}`);
+});
