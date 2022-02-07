2ee2b94675ea37db08edfe6ac5b18b2c7e44fe4f
Joe Mitzman <joemitz@gmail.com>
2022-02-07 14:59:41 -0800

    Made requested changes

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-import ProductOverview from '../components/product_overview/overview.jsx';
-import QuestionsAndAnswers from '../components/questions_and_answers/q&a.jsx';
-import RatingsAndReviews from '../components/ratings_and_reviews/ratings&reviews.jsx';
-import RelatedItemsOutfitCreation from '../components/related_items_outfit_creation/related_items&outfit.jsx';
+import ProductOverview from './product_overview/overview.jsx';
+import QuestionsAndAnswers from './questions_and_answers/q&a.jsx';
+import RatingsAndReviews from './ratings_and_reviews/ratings&reviews.jsx';
+import RelatedItemsOutfitCreation from './related_items_outfit_creation/related_items&outfit.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -20,7 +20,7 @@ class App extends React.Component {
 
 
   render() {
-    return <div></div>;
+    return ( <div></div> );
   }
 }
 
