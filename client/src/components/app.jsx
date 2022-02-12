def09cbbaa2cb71ad6831eb60301b9850d66d119
Caitlin Winters <caitlin.p.winters@gmail.com>
2022-02-11 20:00:37 -0500

    Added dummy data for product and styles implemented getProduct and getStyles from imported dummy data and addToMyOutfit and removeFromMyOutfit. Also removed StarRating.jsx traces of it from app.jsx

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -5,16 +5,54 @@ import ProductOverview from './product_overview/overview.jsx';
 import QuestionsAndAnswers from './questions_and_answers/q&a.jsx';
 import RatingsAndReviews from './ratings_and_reviews/ratings&reviews.jsx';
 import RelatedItemsOutfitCreation from './related_items_outfit_creation/related_items&outfit.jsx';
+import { product, styles } from '../sampleData/sampleData.js';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-
+      product: {},
+      styles: {},
+      myOutfits: []
     };
   }
 
+  componentDidMount() {
+    this.getProducts();
+    this.getStyles();
+    this.addToMyOutfit('1');
+    this.removeFromMyOutfit('1');
+  }
+
+  getProducts() {
+    this.setState({
+      product: product
+    });
+  }
+
+  getStyles() {
+    this.setState({
+      styles: styles
+    });
+  }
+
+  addToMyOutfit(id) {
+    this.setState({
+      myOutfits: this.state.myOutfits.push(id)
+    });
+  }
+
+  removeFromMyOutfit(id) {
+    let index = this.state.myOutfits.indexOf(id);
+    let copy = this.state.myOutfits;
+    copy.splice(index, 1);
+    this.setState({
+      myOutfits: copy
+    }, () => {
+      console.log(this.state.myOutfits);
+    });
+  }
 
   render() {
     return (
