6452d0c284c9c3b4c768b67f04fb0b97207f20a8
Caitlin Winters <caitlin.p.winters@gmail.com>
2022-02-13 12:21:34 -0500

    Made some changes to dummy data

--- client/src/sampleData/sampleData.js
+++ client/src/sampleData/sampleData.js
@@ -1,6 +1,6 @@
 module.exports = {
   product: {
-    'id': 11,
+    'id': 1,
     'name': 'Air Minis 250',
     'slogan': 'Full court support',
     'description': 'This optimized air cushion pocket reduces impact but keeps a perfect balance underfoot.',
@@ -28,12 +28,47 @@ module.exports = {
         'default?': true,
         'photos': [
           {
-            'thumbnail_url': 'urlplaceholder/style_1_photo_number_thumbnail.jpg',
-            'url': 'urlplaceholder/style_1_photo_number.jpg'
+            'thumbnail_url': 'https://i.ibb.co/XYsPZKR/image-b209c84a-736d-44e8-9e90-a11923094bef-110x110-2x.jpg',
+            'url': 'https://cdn.shopify.com/s/files/1/2325/0327/products/image_b209c84a-736d-44e8-9e90-a11923094bef_1024x1024@2x.jpg?v=1623509962'
           },
           {
-            'thumbnail_url': 'urlplaceholder/style_1_photo_number_thumbnail.jpg',
-            'url': 'urlplaceholder/style_1_photo_number.jpg'
+            'thumbnail_url': 'https://i.ibb.co/kq7wjYq/image-209a9c7e-bdac-484b-8965-5aaf99d27cfb-110x110-2x.jpg',
+            'url': 'https://cdn.shopify.com/s/files/1/2325/0327/products/image_209a9c7e-bdac-484b-8965-5aaf99d27cfb_110x110@2x.jpg?v=1623509961'
+          },
+          {
+            'thumbnail_url': 'https://i.ibb.co/0fhKxDv/image-188d1988-d6be-4a43-8a74-8ff7b5633a92-110x110-2x.jpg',
+            'url': 'https://cdn.shopify.com/s/files/1/2325/0327/products/image_188d1988-d6be-4a43-8a74-8ff7b5633a92_110x110@2x.jpg?v=1623509962'
+          },
+        ],
+        'skus': {
+          '37': {
+            'quantity': 8,
+            'size': 'XS'
+          },
+          '38': {
+            'quantity': 16,
+            'size': 'S'
+          },
+          '39': {
+            'quantity': 17,
+            'size': 'M'
+          },
+        }
+      },
+      {
+        'style_id': 2,
+        'name': 'Forest Green & Black',
+        'original_price': '140',
+        'sale_price': '0',
+        'default?': true,
+        'photos': [
+          {
+            'thumbnail_url': 'https://i.ibb.co/gFmRxrQ/camo-two-thumbnail-copy.jpg',
+            'url': 'https://oldnavy.gap.com/webcontent/0019/968/946/cn19968946.jpg'
+          },
+          {
+            'thumbnail_url': 'https://i.ibb.co/gFmRxrQ/camo-two-thumbnail-copy.jpg',
+            'url': 'https://oldnavy.gap.com/webcontent/0019/968/946/cn19968946.jpg'
           }
 
         ],
