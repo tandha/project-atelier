605a9ddf2bfb709e8f381af523577f5f69e1fd1c
Caitlin Winters <caitlin.p.winters@gmail.com>
2022-02-17 19:22:37 -0500

    Added anchor tag to link to ratings and reviews from product information

--- client/src/components/product_overview/productInformation/information.jsx
+++ client/src/components/product_overview/productInformation/information.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import QuestionsAndAnswers from '../../questions_and_answers/q&a.jsx';
 import Feature from './featureInformation.jsx';
 
 const Information = (props) => {
   return (
     <div id='product-information'>
+      <a href="#ratings-and-reviews">Read All Reviews</a><br />
       <div id='star-rating'>{props.starRating}</div>
       <div id='product-category'>{props.product.category}</div>
       <div id='product-name'>{props.product.name}</div>
@@ -17,3 +19,21 @@ const Information = (props) => {
 };
 
 export default Information;
+
+// import React from 'react';
+// import { Link } from 'react-router';
+
+// class List extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <p>Please choose a repository from the list below.</p>
+//                 <ul>
+//                     <li><Link to="/react">React</Link></li>
+//                 </ul>
+//             </div>
+//         );
+//     }
+// }
+
+// export default List;
