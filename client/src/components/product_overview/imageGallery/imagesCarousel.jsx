a12f3d740da805bc4e49a15729f4bb922153db99
Caitlin Winters <caitlin.p.winters@gmail.com>
2022-02-25 12:47:48 -0500

    Added arrows for navitaing through main images

--- client/src/components/product_overview/imageGallery/imagesCarousel.jsx
+++ client/src/components/product_overview/imageGallery/imagesCarousel.jsx
@@ -1,10 +1,14 @@
 // import React from 'react';
 import React, { useState, useEffect } from 'react';
 import Thumbnails from './imageThumbnails.jsx';
+import { IoMdArrowBack, IoMdArrowForward } from 'react-icons/io';
+
 const ImageCarousel = (props) => {
 
   return (
     <div id='image-gallery'>
+      <IoMdArrowBack id='image-gallery-left-arrow'/>
+      <IoMdArrowForward id='image-gallery-right-arrow' />
       <img data-testid="main-image" id='main-image' src={props.photos[props.mainPhotoIndex].url}></img>
       <Thumbnails photos={props.photos} mainIndex={props.mainPhotoIndex} changePhoto={props.changePhoto}/>
     </div>
