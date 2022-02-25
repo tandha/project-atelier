fb7b0bad103c80025c2a6c9e002abf7ed4581cca
Caitlin Winters <caitlin.p.winters@gmail.com>
2022-02-25 14:17:47 -0500

    Selected thumbnail displays highlight bar

--- client/src/components/product_overview/imageGallery/imageThumbnail.jsx
+++ client/src/components/product_overview/imageGallery/imageThumbnail.jsx
@@ -4,6 +4,9 @@ const Thumbnail = (props) => {
   return (
     <li>
       <img src={props.thumbnail} id={props.i} className='gallery-thumbnail' onClick={props.changePhoto.bind(this)}></img>
+      {
+        props.mainIndex === props.i && <div id='thumbnail-highlight-bar'></div>
+      }
     </li>
   );
 };
--- client/src/components/product_overview/imageGallery/imageThumbnails.jsx
+++ client/src/components/product_overview/imageGallery/imageThumbnails.jsx
@@ -6,7 +6,7 @@ const Thumbnails = (props) => {
     <ul id='gallery-thumbnails'>
       {
         props.photos.map((photo, i) => {
-          return <Thumbnail key={i} i={i} thumbnail={photo.thumbnail_url} changePhoto={props.changePhoto}/>;
+          return <Thumbnail key={i} i={i} mainIndex={props.mainIndex} thumbnail={photo.thumbnail_url} changePhoto={props.changePhoto}/>;
         })
       }
     </ul>
