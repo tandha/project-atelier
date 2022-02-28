c7ab3082642bbc1eb64f2046f042cc35ecca484f
yfcheng1 <b99501048@gmail.com>
2022-02-28 11:38:40 -0500

    fix minor note

--- client/src/components/questions_and_answers/Question.jsx
+++ client/src/components/questions_and_answers/Question.jsx
@@ -69,7 +69,6 @@ class Question extends React.Component {
     let nickname = e.target[1].value;
     let email = e.target[2].value;
     let images = document.querySelectorAll('#QA-preview');
-    // const images = useRef....
 
     let imageURL = [];
     images.forEach((image) => (
