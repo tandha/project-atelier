1243d37a6ae52d8a2d173fa7e2dd32db54b43704
Joe Mitzman <joemitz@gmail.com>
2022-02-27 22:59:56 -0800

    char-ratings css working

--- client/src/components/ratings_and_reviews/rating-breakdown/styles.jsx
+++ client/src/components/ratings_and_reviews/rating-breakdown/styles.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { VscTriangleDown } from 'react-icons/vsc';
 
 const Styles = (props) => {
 
   let styles = {};
   let styleKeys = [];
   let styleVals = [];
+
+  let charsScale = {
+    Size: ['A size too small', '½ a size too small', 'Perfect', '½ a size too big', 'A size too wide'],
+    Width: ['Too narrow', 'Slightly narrow', 'Perfect', 'Slightly wide', 'Too wide'],
+    Comfort: ['Uncomfortable', 'Slightly uncomfortable', 'Ok', 'Comfortable', 'Perfect'],
+    Quality: ['Quality', 'Poor', 'Below average', 'What I expected', 'Pretty great', 'Perfect'],
+    Length: ['Runs Short', 'Runs slightly short', 'Perfect', 'Runs slightly long', 'Runs long'],
+    Fit: ['Runs tight', 'Runs slightly tight', 'Perfect', 'Runs slightly long', 'Runs long']
+  };
 
   if (props.metaData.data) {
     styles = props.metaData.data.characteristics;
@@ -16,11 +26,21 @@ const Styles = (props) => {
 
   return (
     <div id='ratings-breakdown-styles'>
-      <br></br>
       {
         styleKeys.map((style, index) => {
           return (
-            <div key={index}>{style}: {Number.parseFloat(styleVals[index]).toFixed(1)}</div>
+            <React.Fragment key={index}>
+              <div className='chars-name'>{style}</div>
+              <VscTriangleDown size={28} color={'gray'} className='chars-icon'
+                style={{left: (32 * Number.parseFloat(styleVals[index])).toFixed()}}
+              />
+              <div className='chars-bar'></div>
+              <div className='chars-list'>
+                <span className='char-left'>{charsScale[style][0]}</span>
+                <span className='char-right'>{charsScale[style][4]}</span>
+              </div>
+
+            </React.Fragment>
           );
         })
       }
