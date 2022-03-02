685545c7903115348779e2cfbf239f484f18a798
yfcheng1 <b99501048@gmail.com>
2022-03-02 13:05:51 -0500

    add feature tests

--- client/src/components/questions_and_answers/test/test.js
+++ client/src/components/questions_and_answers/test/test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent, cleanup } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import 'regenerator-runtime/runtime.js';
 import axios from 'axios';
@@ -14,20 +14,13 @@ import AnswerPhoto from '../AnswerPhoto.jsx';
 import QuestionModal from '../ModalForm/QuestionModal.jsx';
 import AnswerModal from '../ModalForm/AnswerModal.jsx';
 
-const request = require('supertest');
-const { API_URL, API_KEY } = require('../../../../../server/config.js');
-axios.defaults.baseURL = API_URL;
-
 /**
  * Render tests
  */
 describe('Q&A render tests', () => {
   var props = {};
-
   beforeEach(() => {
     props = {
-      productID: exampleQuestions.product.id,
-      productName: exampleQuestions.product.name,
       product: exampleQuestions.product,
       questions: exampleQuestions.results
     };
@@ -36,7 +29,7 @@ describe('Q&A render tests', () => {
   afterEach(cleanup);
 
   test('render Q&A main page', () => {
-    render(<QuestionsAndAnswers {...props}/>);
+    render(<QuestionsAndAnswers product={props.product} questions={props.questions}/>);
   });
 
   test('render searchBar component', () => {
@@ -80,32 +73,30 @@ describe('Q&A render tests', () => {
  * more widget feature tests
  */
 
-describe.only('Test on widget features', () => {
+describe('Test on search bar component', () => {
+
+  test('test on searchBar component', () => {
+    let props = {
+      product: exampleQuestions.product,
+      questions: exampleQuestions.results
+    };
+
+    const {queryByPlaceholderText} = render(<QuestionsAndAnswers {...props}/>);
+    const searchInput = queryByPlaceholderText('HAVE A QUESTION? SEARCH FOR ANSWERS...');
+    fireEvent.change(searchInput, { target: { value: 'test'} });
+    expect(searchInput.value).toBe('test');
+  });
+
+});
+
+describe('Test on Question Features', () => {
 
   var props = {};
   beforeEach(() => {
     props = {
-      productID: exampleQuestions.product.id,
-      productName: exampleQuestions.product.name,
       product: exampleQuestions.product,
       questions: exampleQuestions.results
     };
-  });
-
-  afterEach(cleanup);
-
-  test('render Q&A main page', () => {
-    render(<QuestionsAndAnswers {...props}/>);
-
-  });
-
-  test('test on searchBar component', () => {
-    const setSearch = jest.fn((value) => {});
-    const {queryByPlaceholderText} = render(<SearchBar handleSearchBar={setSearch}/>);
-
-    const searchInput = queryByPlaceholderText('HAVE A QUESTION? SEARCH FOR ANSWERS...');
-    fireEvent.change(searchInput, { target: { value: 'test'} });
-    expect(searchInput.value).toBe('test');
   });
 
   test('test on QuestionList component', () => {
@@ -119,10 +110,20 @@ describe.only('Test on widget features', () => {
     // test on sort by helpfulness
   });
 
-
   test('test on Question component', () => {
     render(<Question question={props.questions[1]}/>);
     // test on click more answers event
+  });
+
+});
+
+describe('Test on Answer Features', () => {
+  var props = {};
+  beforeEach(() => {
+    props = {
+      product: exampleQuestions.product,
+      questions: exampleQuestions.results
+    };
   });
 
   test('render AnswerList component', () => {
@@ -145,202 +146,47 @@ describe.only('Test on widget features', () => {
     // test on click report event
   });
 
-  test('render Answer photo component', () => {
-    render(<QuestionModal productName={props.productName}/>);
-  });
-
-  test('render Question modal form component', () => {
+  test('render Answer photo form component', () => {
     const answerArray = Object.values(props.questions[2].answers);
     const photo = answerArray[0].photos[0];
     render(<AnswerPhoto photo={photo}/>);
   });
-
 });
 
+describe.only('Test on Modal Form Features', () => {
+
+  var props = {};
+  beforeEach(() => {
+    props = {
+      product: exampleQuestions.product,
+      questions: exampleQuestions.results
+    };
+  });
+
+  test('Test on click add question', () => {
+    render(<QuestionsAndAnswers {...props}/>);
+    const btn = screen.getByText('ADD A QUESTION');
+    fireEvent.click(btn);
+    const form = document.querySelector('#new-question-modal');
+    expect(form).toBeInTheDocument();
+
+  });
+
+  test('Test on question modal', () => {
+    const testData = {body: 'Can I wash it?', nickname: 'tobi', email: 'tobi@email.com'};
+
+    render(<QuestionModal productName={props.productName}/>);
+  });
+
+  test('Test on answer modal', () => {
+    render(<AnswerModal productName={props.productName} question={props.questions[1]}/>);
+  });
+});
 
 /**
- * Test on End points
+ * Error boundary tests
  */
 
-describe('Test on API end points', () => {
-
-  let testProductID = 64622;
-  let testQuestionID = 573794;
-
-  afterEach(cleanup);
-
-  test('should get questions from API', async () => {
-    const result = await request(API_URL)
-      .get('/qa/questions')
-      .set('Authorization', API_KEY)
-      .query({'product_id': testProductID, count: 100})
-      .catch((err) => console.log('err on GET', err));
-
-    expect(result.status).toBe(200);
-    expect(typeof result.body).toBe('object');
-    expect(Array.isArray(result.body.results)).toBe(true);
-  });
-
-  test('should post questions to API ', async () => {
-    let data = {
-      'body': 'I have a question',
-      'name': 'tobi',
-      'email': 'tobi@gmail.com',
-      'product_id': testProductID
-    };
-
-    const postResult = await request(API_URL)
-      .post('/qa/questions')
-      .set('Authorization', API_KEY)
-      .send(data)
-      .catch((err) => console.log('err on POST questions', err));
-
-    const getresult = await request(API_URL)
-      .get('/qa/questions')
-      .set('Authorization', API_KEY)
-      .query({'product_id': data.product_id, count: 100})
-      .catch((err) => console.log('err on GET', err));
-
-    expect(postResult.status).toBe(201);
-    expect(getresult.status).toBe(200);
-
-    let result = getresult.body.results;
-    let newQuestion = {};
-    for (var i = 0; i < result.length; i++) {
-      if (result[i].asker_name === data.name) {
-        newQuestion = result[i];
-      }
-    }
-    expect(newQuestion.question_body).toContain(data.body);
-    expect(newQuestion.asker_name).toContain(data.name);
-  });
-
-
-  test('should post answers to API ', async () => {
-    let data = {
-      'body': 'I am not sure',
-      'name': 'tobi',
-      'email': 'tobi@gmail.com',
-      'photos': []
-    };
-
-    const result = await request(API_URL)
-      .post(`/qa/questions/${testQuestionID}/answers`)
-      .set('Authorization', API_KEY)
-      .send(data)
-      .catch((err) => console.log('err on POST answers', err));
-
-    expect(result.status).toBe(201);
-  });
-
-
-  test('should mark question helpful and verify increase helpfulness', async () => {
-    var firstCount = 0;
-    var updateCount = 0;
-
-    const getRequest = await request(API_URL)
-      .get('/qa/questions')
-      .set('Authorization', API_KEY)
-      .query({'product_id': testProductID, count: 100})
-      .catch((err) => console.log('err on GET', err));
-
-    expect(getRequest.status).toBe(200);
-    let result1 = getRequest.body.results;
-    for (var i = 0; i < result1.length; i++) {
-      if (result1[i].question_id === testQuestionID) {
-        firstCount = result1[i].question_helpfulness;
-      }
-    }
-
-    const putRequest = await request(API_URL)
-      .put(`/qa/questions/${testQuestionID}/helpful`)
-      .set('Authorization', API_KEY)
-      .catch((err) => console.log('err on PUT', err));
-
-    expect(putRequest.status).toBe(204);
-
-    const newGetRequest = await request(API_URL)
-      .get('/qa/questions')
-      .set('Authorization', API_KEY)
-      .query({'product_id': testProductID, count: 100})
-      .catch((err) => console.log('err on GET', err));
-
-    expect(newGetRequest.status).toBe(200);
-    let result2 = newGetRequest.body.results;
-    for (var i = 0; i < result2.length; i++) {
-      if (result2[i].question_id === testQuestionID) {
-        updateCount = result2[i].question_helpfulness;
-      }
-    }
-
-    expect(updateCount).toEqual(firstCount + 1);
-  });
-
-  test('should mark answer helpful and verify increase helpfulness', async () => {
-    var firstCount = 0;
-    var updateCount = 0;
-    var firstAnswerID = 0;
-    var newAnwerID = 0;
-
-    const getRequest = await request(API_URL)
-      .get(`/qa/questions/${testQuestionID}/answers`)
-      .set('Authorization', API_KEY)
-      .catch((err) => console.log('err on GET', err));
-
-    expect(getRequest.status).toBe(200);
-    firstAnswerID = getRequest.body.results[0].answer_id;
-    firstCount = getRequest.body.results[0].helpfulness;
-
-    const putRequest = await request(API_URL)
-      .put(`/qa/answers/${firstAnswerID}/helpful`)
-      .set('Authorization', API_KEY)
-      .catch((err) => console.log('err on PUT', err));
-
-    expect(putRequest.status).toBe(204);
-
-    const newGetRequest = await request(API_URL)
-      .get(`/qa/questions/${testQuestionID}/answers`)
-      .set('Authorization', API_KEY)
-      .catch((err) => console.log('err on GET', err));
-
-    expect(newGetRequest.status).toBe(200);
-    newAnwerID = newGetRequest.body.results[0].answer_id;
-    updateCount = newGetRequest.body.results[0].helpfulness;
-
-    expect(newAnwerID).toEqual(firstAnswerID);
-    expect(updateCount).toEqual(firstCount + 1);
-  });
-
-  test('should report an answer and verify being reported', async () => {
-    var firstAnswerID = 0;
-
-    const getRequest = await request(API_URL)
-      .get(`/qa/questions/${testQuestionID}/answers`)
-      .set('Authorization', API_KEY)
-      .catch((err) => console.log('err on GET', err));
-
-    expect(getRequest.status).toBe(200);
-    firstAnswerID = getRequest.body.results[0].answer_id;
-
-    const putRequest = await request(API_URL)
-      .put(`/qa/answers/${firstAnswerID}/report`)
-      .set('Authorization', API_KEY)
-      .catch((err) => console.log('err on PUT', err));
-
-    expect(putRequest.status).toBe(204);
-
-    const main = async () => {
-      await getRequest;
-      await putRequest;
-
-      const newGetRequest = await request(API_URL)
-        .get(`/qa/questions/${testQuestionID}/answers`)
-        .set('Authorization', API_KEY)
-        .catch((err) => console.log('err on GET', err));
-
-      expect(newGetRequest.status).toBe(200);
-      expect(newGetRequest.body.results.length).toEqual(getRequest.body.results.length - 1);
-    };
-  });
+describe('Test on errors', () => {
 
 });
