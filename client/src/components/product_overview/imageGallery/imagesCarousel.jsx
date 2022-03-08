84a2faf137db1ab3af0f3a029e89772418638140
Caitlin Winters <caitlin.p.winters@gmail.com>
2022-03-08 14:00:26 -0500

    Removed console.log

--- client/src/components/product_overview/imageGallery/imagesCarousel.jsx
+++ client/src/components/product_overview/imageGallery/imagesCarousel.jsx
@@ -32,7 +32,6 @@ class ImageCarousel extends React.Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    console.log('what is this', this.props.selectedStyle);
     if (prevProps.selectedStyle !== this.props.selectedStyle) {
       this.setState({
         mainPhotoIndex: prevState.mainPhotoIndex,
