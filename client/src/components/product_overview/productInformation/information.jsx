c3b14d43c8f12251fb8509a92b28ad40fed99116
Caitlin Winters <caitlin.p.winters@gmail.com>
2022-03-10 23:29:25 -0500

    Toggle working, added some styles WIP

--- client/src/components/product_overview/productInformation/information.jsx
+++ client/src/components/product_overview/productInformation/information.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import { BsToggleOff, BsToggleOn } from 'react-icons/bs';
 
 const Information = (props) => {
+  let darkMode = false;
+  const toggleMode = () => {
+    const app = document.getElementById('app');
+    darkMode = !darkMode;
+    console.log('dark mode', darkMode);
+    if (darkMode) {
+      app.classList.add('dark-mode-app');
+    } else {
+      app.classList.remove('dark-mode-app');
+      console.log('what is it', app);
+    }
+  };
   return (
     <div id='product-information'>
+      <div id='toggle-mode'>
+        {darkMode ? <BsToggleOff onClick={toggleMode.bind(this)} /> : <BsToggleOn onClick={toggleMode.bind(this)}/>}
+      </div>
       <div id='star-rating'>{props.starRating}</div>
       <a id='reviews' href="#ratings-and-reviews">Read All Reviews</a>
       <div id='product-category'>{props.product.category}</div>
